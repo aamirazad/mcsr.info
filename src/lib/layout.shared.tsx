96a5e2266d49ae220464c6e27f0c26d162ef8672
Aamir Azad <aamirmazad@gmail.com>
2026-02-24 21:08:49 -0500

    Add message about wiki being work in progress

--- src/lib/layout.shared.tsx
+++ src/lib/layout.shared.tsx
@@ -10,7 +10,7 @@ export const gitConfig = {
 export function baseOptions(): BaseLayoutProps {
   return {
     nav: {
-      title: "mcsr.info",
+      title: "MCSR Info",
     },
     githubUrl: `https://github.com/${gitConfig.user}/${gitConfig.repo}`,
   };
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,4 +1,4 @@
-import { createFileRoute, Link } from "@tanstack/react-router";
+import { createFileRoute } from "@tanstack/react-router";
 import { HomeLayout } from "fumadocs-ui/layouts/home";
 import { baseOptions } from "@/lib/layout.shared";
 
@@ -10,8 +10,18 @@ function Home() {
   return (
     <HomeLayout {...baseOptions()}>
       <div className="flex flex-col flex-1 justify-center px-4 py-8 text-center">
-        <h1 className="font-medium text-xl mb-4">mcsr.info Knowledge Base</h1>
-        <Link
+        <h1 className="font-medium text-xl mb-4">
+          Minecraft Speedrunning Knowledge Base
+        </h1>
+        <div className="flex justify-center">
+          <p className="max-w-2xl">
+            This community made wiki will be a central database for everything
+            there is to know about speedrunning Minecraft. The site is currently
+            a work in progress, feel free to contribute on GitHub.
+          </p>
+        </div>
+
+        {/* <Link
           to="/wiki/$"
           params={{
             _splat: "",
@@ -19,7 +29,7 @@ function Home() {
           className="px-3 py-2 rounded-lg bg-fd-primary text-fd-primary-foreground font-medium text-sm mx-auto"
         >
           Open Wiki
-        </Link>
+        </Link> */}
       </div>
     </HomeLayout>
   );
